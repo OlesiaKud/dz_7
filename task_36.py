33c3574f4d0c9714b450a42531b98fd02fc0855e
Olesya Kudr <ala.kud00@mail.ru>
2023-09-15 09:21:39 +0700

    task 36

---
+++ task_36.py
@@ -0,0 +1,38 @@
+# Задача 36: Напишите функцию print_operation_table(operation, num_rows=6, num_columns=6), 
+# которая принимает в качестве аргумента функцию, вычисляющую элемент по номеру строки и столбца.
+# Аргументы num_rows и num_columns указывают число строк и столбцов таблицы, которые должны быть 
+# распечатаны. Нумерация строк и столбцов идет с единицы (подумайте, почему не с нуля). 
+# Примечание: бинарной операцией называется любая операция, у которой ровно два аргумента, 
+# как, например, у операции умножения.
+# # *Пример:*
+# **Ввод:** `print_operation_table(lambda x, y: x * y) ` 
+# **Вывод:**
+#  1   2   3    4   5   6
+#  2   4   6    8  10  12
+#  3   6   9   12  15  18
+#  4   8  12   16  20  24
+#  5  10  15   20  25  30
+#  6  12  18   24  30  36
+
+
+def print_operation_table(operation, num_rows=6, num_columns=6):
+    res = [[operation(i, j) for j in range(1, num_columns + 1 )] for i in range(1, num_rows + 1)]
+    for k in res:
+        print(*[f"{x:>4}" for x in k])
+        
+print('Выберите операцию, которую хотите произвести: \n 1, если x + y, \n 2, если x - y,\n 3, если x * y,\n 4, если x / y')
+oper = int(input(': '))
+match oper:
+    case 1:
+        print(print_operation_table(lambda x, y: x + y))
+    case 2:
+        print(print_operation_table(lambda x, y: x - y))
+    case 3:
+        print(print_operation_table(lambda x, y: x * y))   
+    case 4:
+        print(print_operation_table(lambda x, y: x / y))
+# print_operation_table(lambda x, y: x * y)  
+     
+
+
+
